08f635ad41987360d6e18089b51451e44732342a
abhiram edupuganti <abhiramedupuganti@gmail.com>
2024-10-21 17:33:39 +0530

    added add method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World , this is about git");
+        
+    }
+
+    public static void add(int x,int y)
+    {
+        System.out.println(x+y);
     }
 }
